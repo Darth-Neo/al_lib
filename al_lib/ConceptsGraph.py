f850f3a5bf4ee11ba16a1096da0d2d35b618f295
DarthNeo <morrissoid.james@hotmail.com>
2015-06-16 20:09:06 -0400

    Updates hurt

--- al_lib/ConceptsGraph.py
+++ al_lib/ConceptsGraph.py
@@ -9,90 +9,42 @@ import os
 from Logger import *
 logger = setupLogging(__name__)
 logger.setLevel(INFO)
+
+from nl_lib.Constants import *
 from nl_lib.Concepts import Concepts
-from nl_lib.ConceptGraph import PatternGraph, GraphVizGraph
+from nl_lib.ConceptGraph import PatternGraph, GraphVizGraph, NetworkXGraph
+
 from Constants import *
-import pytest
 
-class ConceptsGraph(object):
-
-    def __init__(self, graph=None, fileImage=None):
-        self.threshold=0.0005
-
-        if graph is None:
-            # self.graph = PatternGraph()
-            self.graph = GraphVizGraph()
-        else:
-            self.graph = graph
-
-        if fileImage is None:
-            self.fileImage = u"example.png"
-        else:
-            self.fileImage = fileImage
-
-        #
-        # Hack to get GraphViz to work
-        #
-        os.environ[u'PATH'] = u"%s:/opt/local/bin" % os.environ[u'PATH']
-
-    def addGraphNodes(self, concepts, n=0):
-
+def logGraph(gl, title, scale=1):
+    logger.info(u"---%s---" % title)
+    n = 0
+    for x in gl:
         n += 1
+        if isinstance(gl, dict):
+            logger.info(u"%s [%d]:%s=%3.4f" % (title, n, x, gl[x]*scale))
 
-        for c in concepts.getConcepts().values():
-            logger.debug(u"%d : %d Node c : %s:%s" % (n, len(c.getConcepts()), c.name, c.typeName))
+        else:
+            logger.info(u"%s [%d]" % (x, n))
 
-            self.graph.addConcept(c)
+def graphConcepts(graph, conceptFile):
 
-            if len(c.getConcepts()) > self.threshold:
-                self.addGraphNodes(c, n)
+    concepts = Concepts.loadConcepts(conceptFile)
+    # concepts.logConcepts()
 
-    def addGraphEdges(self, concepts, n=0):
-        n += 1
+    graph.addGraphNodes(concepts)
+    graph.addGraphEdges(concepts)
 
-        self.graph.addConcept(concepts)
+    if isinstance(graph, NetworkXGraph):
+        graph.saveJSON(concepts)
 
-        for c in concepts.getConcepts().values():
+    if isinstance(graph, GraphVizGraph):
+        graph.exportGraph()
 
-            logger.debug(u"%d : %d %s c : %s:%s" % (n, len(c.getConcepts()), concepts.name, c.name, c.typeName))
+    if isinstance(graph, PatternGraph):
+        graph.exportGraph()
 
-            self.graph.addConcept(c)
-
-            self.graph.addEdge(concepts, c)
-
-            if len(c.getConcepts()) != 0:
-                self.addGraphEdges(c, n)
-
-    def conceptsGraph(self, concepts):
-
-        logger.info(u"Adding %s nodes the graph ..." % type(self.graph))
-        self.addGraphNodes(concepts)
-
-        logger.info(u"Adding %s edges the graph ..." % type(self.graph))
-        self.addGraphEdges(concepts)
-
-        if isinstance(self.graph, GraphVizGraph):
-            self.graph.exportGraph(filename=self.fileImage)
-            logger.info(u"Saved Graph - %s" % self.fileImage)
-
-        if isinstance(self.graph, PatternGraph):
-            # graph.g.remove("ProjectConceptsSimilarity")
-            logger.info(u"Exporting Graph")
-            self.graph.exportGraph()
-
-    def logGraph(self, gl, title, scale=1):
-        logger.info(u"---%s---" % title)
-        n = 0
-        for x in gl:
-            n += 1
-            if isinstance(gl, dict):
-                logger.info(u"%s [%d]:%s=%3.4f" % (title, n, x, gl[x]*scale))
-
-            else:
-                logger.info(u"%s [%d]" % (x, n))
-
-
-def test_ConceptsGraph():
+def test_ConceptsGraph(graph):
 
     conceptFile = fileConceptsNGramsSubject
 
@@ -100,14 +52,15 @@ def test_ConceptsGraph():
         logger.info(u"File does not exist : %s" % conceptFile)
 
     else:
-        concepts = Concepts.loadConcepts(conceptFile)
-
-        cg = ConceptsGraph(fileImage=fileImageExport)
-
-        cg.conceptsGraph(concepts)
+        graphConcepts(graph, conceptFile)
 
 if __name__ == u"__main__":
-    test_ConceptsGraph()
+
+    # graph = PatternGraph()
+    graph = GraphVizGraph()
+    # graph = NetworkXGraph(conceptFile[:-2]+u".png")
+
+    test_ConceptsGraph(graph)
 
 
 
